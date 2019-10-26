bd2ee83091500d0b89de8c1cc38da91ea823c034
Иван Иван <remillius_dies@outlook.com>
2019-10-26 11:54:36 +0300

    params

--- lec26oct/Program.cs
+++ lec26oct/Program.cs
@@ -10,7 +10,27 @@ namespace lec26oct
     {
         static void Main(string[] args)
         {
+            // class    value   ref
+            // struct   value   ref
+            int[] ar = new[] {4, 5, 6};
+
+            int i = Add(0,ar);
+            Console.WriteLine(Add(Add(4),Add(4)));
             
+            //demo params
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x, params int[] others)
+        {
+            int result = x;
+            for (int i = 0; i < others.Length; i++)
+            {
+                result+= others[i];
+            }
+            return result;
         }
     }
 }
+
