0360969b1335e073b4ca8bc45fcd5f4348335e91
Иван Иван <remillius_dies@outlook.com>
2019-10-26 14:07:15 +0300

    string demo add

--- lec26oct/Program.cs
+++ lec26oct/Program.cs
@@ -13,11 +13,17 @@ namespace lec26oct
             // class    value   ref
             // struct   value   ref
             int[] ar = new[] {4, 5, 6};
-
+            
             int i = Add(0,ar);
             Console.WriteLine(Add(Add(4),Add(4)));
             
             //demo params
+
+
+            //demo string
+
+            
+            
 
             Console.ReadLine();
         }
@@ -31,6 +37,20 @@ namespace lec26oct
             }
             return result;
         }
+
+        static string CreateSqlQuery(int[] ids)
+        {
+            string result = "select * from Orders where ID=";
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                result += ids[i] + ", ";
+            }
+
+            result = result.Remove(result.Length - 2, 2);
+            return result;
+        }
+
     }
 }
 
